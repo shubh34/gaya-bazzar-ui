769f513ec8b52f631252b34dc9919eddec784a8c
Subham Jain <Subham_Jain@syntelinc.com>
2019-01-28 22:59:19 +0000

    Eslint config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -7,34 +7,34 @@ module.exports = {
   devtool: 'inline-source-map',
   module: {
     rules: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
-            }
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
-        },
-    ]
-},
-plugins: [
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Gaya Bazzar',
       template: './dev/index.ejs',
       inject: 'body',
-    })
+    }),
   ],
-output: {
+  output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 9000
-  }
+    port: 9000,
+  },
 };
