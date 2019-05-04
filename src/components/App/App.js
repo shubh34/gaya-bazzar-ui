9d8d3417b4820599dc6a1a4c1ec23c8e30c2372d
Shubham Jain <shubhamjain.qwerty@gmail.com>
2019-05-04 11:11:27 +0100

    Apply devtool

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import createStore from '../../store';
+import configureStore from '../../store';
 
-const store = createStore(['test']);
+const store = configureStore(['test']);
 
 const App = () => (
 	<Provider store={store}>
